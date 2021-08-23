90113b0a2f10c95df03baf6aade87821d0223d39
gaston coria <coria.cs@gmail.com>
2021-08-23 12:51:03 -0300

    se agrego las preguntas y respuestas

--- roket/apps/pregunta/admin.py
+++ roket/apps/pregunta/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
-from .models import Pregunta
+from .models import Pregunta, Respuesta
 # Register your models here.
 
-admin.site.register(Pregunta)
+class ElegirRespuestaInline(admin.TabularInline):
+	model = ElegirRespuesta
+
+class PreguntaAdmin(admin.ModelAdmin):
+	model = Preguntas
+	inlines = (ElegirRespuestaInline, )
+	list_display = ['la_pregunta']
+	search_fields = ['texto', 'respuestas__texto']
+
+
+admin.site.register(Pregunta, PreguntaAdmin)
+admin.site.register(Respuesta)
+
+
--- roket/apps/pregunta/models.py
+++ roket/apps/pregunta/models.py
@@ -5,6 +5,17 @@ from apps.core.models import TimeModel
 
 
 class Pregunta(TimeModel):
-	la_pregunta = models.CharField(max_length = 200 )
+	la_pregunta = models.TextField(verbose_name='Texto de la pregunta' )
 	
+	def __str__(self):
+		return self.la_pregunta
 
+class Respuesta(models.Model):
+
+	respuesta = models.ForeignKey(Pregunta, related_name='respuestas', on_delete=models.CASCADE)
+	correcta = models.BooleanField(verbose_name='¿Esta seguro de esta respueta?',default=False, null=False)
+	texto = models.TextField(verbose_name='Texto de la respuesta')
+
+
+	def __str__(self):
+		return self.texto
