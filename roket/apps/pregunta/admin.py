22c3b3b1e0ec82d6930f5d4826fb21448f3742b7
gaston coria <coria.cs@gmail.com>
2021-08-23 14:27:48 -0300

    se agrego bien preguntas

--- roket/apps/pregunta/admin.py
+++ roket/apps/pregunta/admin.py
@@ -3,10 +3,10 @@ from .models import Pregunta, Respuesta
 # Register your models here.
 
 class ElegirRespuestaInline(admin.TabularInline):
-	model = ElegirRespuesta
+	model = Respuesta
 
 class PreguntaAdmin(admin.ModelAdmin):
-	model = Preguntas
+	model = Pregunta
 	inlines = (ElegirRespuestaInline, )
 	list_display = ['la_pregunta']
 	search_fields = ['texto', 'respuestas__texto']
