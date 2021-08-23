9df228b43302eb1028fe147e269de32150875f2f
gaston coria <coria.cs@gmail.com>
2021-08-23 14:55:44 -0300

    se enlaza con el usuario para jugar

--- roket/apps/pregunta/admin.py
+++ roket/apps/pregunta/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
-from .models import Pregunta, Respuesta
+from .models import Pregunta, Respuesta, PreguntasRespondidas
+from django.conf import settings
+
+
+
 # Register your models here.
 
 class ElegirRespuestaInline(admin.TabularInline):
 	model = Respuesta
+	can_delete = False
+	max_num = Respuesta.maximo_respuesta
+
 
 class PreguntaAdmin(admin.ModelAdmin):
 	model = Pregunta
@@ -12,6 +19,15 @@ class PreguntaAdmin(admin.ModelAdmin):
 	search_fields = ['texto', 'respuestas__texto']
 
 
+
+class PreguntasRespondidasAdmin(admin.ModelAdmin):
+	list_display = ['pregunta', 'respuesta','correcta','puntaje']
+
+	class meta:
+		model = PreguntasRespondidas
+
+
+admin.site.register(PreguntasRespondidas)
 admin.site.register(Pregunta, PreguntaAdmin)
 admin.site.register(Respuesta)
 
--- roket/apps/pregunta/models.py
+++ roket/apps/pregunta/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from apps.core.models import TimeModel
 # Create your models here.
 
+from django.contrib.auth.models import User
 
 class Pregunta(TimeModel):
 	la_pregunta = models.TextField(verbose_name='Texto de la pregunta' )
@@ -12,6 +13,8 @@ class Pregunta(TimeModel):
 
 class Respuesta(models.Model):
 
+	maximo_respuesta = 3
+
 	respuesta = models.ForeignKey(Pregunta, related_name='respuestas', on_delete=models.CASCADE)
 	correcta = models.BooleanField(verbose_name='¿Esta seguro de esta respueta?',default=False, null=False)
 	texto = models.TextField(verbose_name='Texto de la respuesta')
@@ -19,3 +22,14 @@ class Respuesta(models.Model):
 
 	def __str__(self):
 		return self.texto
+
+class Usuarios(models.Model):
+	usuarios = models.OneToOneField(User, on_delete=models.CASCADE)
+	puntaje_total = models.DecimalField(verbose_name='Puntaje Total', default=0, decimal_places=2, max_digits=10)
+
+class PreguntasRespondidas(models.Model):
+	Usuarios = models.ForeignKey(Usuarios, on_delete=models.CASCADE)
+	pregunta = models.ForeignKey(Pregunta, on_delete=models.CASCADE)
+	respuesta = models.ForeignKey(Respuesta, on_delete=models.CASCADE, related_name='intentos')
+	correcta = models.BooleanField(verbose_name='¿Es tu respuesta correcta?', default=False, null=False)
+	puntaje = models.DecimalField(verbose_name='Su puntaje',default=0, decimal_places=2, max_digits=6)
